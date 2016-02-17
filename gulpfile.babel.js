9e87bc47658e9e452200d711ab3d0dcfc35ab3fd
Thomas Karagianes <thomas@karagianes.com>
2016-02-16 22:35:32 -0800

    Catch-Up Commit
    A few changes made to various styling on multiple pages.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -106,9 +106,7 @@ gulp.task('styles', () => {
 // `.babelrc` file.
 gulp.task('scripts', () =>
     gulp.src([
-      // Note: Since we are not using useref in the scripts build pipeline,
-      //       you need to explicitly list your scripts here in the right order
-      //       to be correctly concatenated
+      './app/plugins/'
       './app/scripts/main.js'
       // Other scripts
     ])
